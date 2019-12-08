d15d0d35ca136df51cc91b90e6fc768aaa4556cd
Eray Erdin <eraygezer.94@gmail.com>
2019-12-08 22:53:01 +0300

    added `value` to `update` method of queryset

--- project/tests/test_managers.py
+++ project/tests/test_managers.py
@@ -9,3 +9,14 @@ class TestOverrideVariableManager:
     def test_create(self, db):
         v = models.Variable.objects.create(name="FOO", value=5)
         assert v.value == 5
+
+    @pytest.mark.it("`update` method")
+    def test_update(self, variable_factory):
+        for i in range(2):
+            variable_factory("bar", "FOO{}".format(i))
+
+        variables = models.Variable.objects.filter(name__startswith="FOO")
+        variables.update(value="baz")
+
+        for v in variables:
+            assert v.value == "baz"
