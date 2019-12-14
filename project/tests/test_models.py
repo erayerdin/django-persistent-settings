61da7bb661bd46816efbc74c84c31698b61c8c4c
Eray Erdin <eraygezer.94@gmail.com>
2019-12-15 01:19:57 +0300

    migrated to custom `_PickleField`

--- project/tests/test_models.py
+++ project/tests/test_models.py
@@ -2,8 +2,7 @@ import pytest
 
 from persistent_settings import models
 
-
-@pytest.mark.describe("Variable Model")
+""" @pytest.mark.describe("Variable Model")
 class TestVariable:
     @pytest.mark.it("`value` prop with types")
     def test_value_prop(self, variable_factory):
@@ -48,3 +47,33 @@ class TestVariable:
 
         for v in variables:
             assert v.value == "baz"
+ """
+# all tests above are invalidated due to it is already being covered by standard
+# implementation of BinaryField, which is implemented in models as `_PickleField`
+# these are subject to removal
+
+
+@pytest.mark.describe("Variable Model")
+class TestVariable:
+    @pytest.mark.it("Create with all types")
+    def test_create(self, db):
+        models.Variable.objects.create(value=5, name="V_INT")
+        models.Variable.objects.create(value=5.5, name="V_FLOAT")
+        models.Variable.objects.create(value=True, name="V_BOOL")
+        models.Variable.objects.create(value="bar", name="V_STR")
+        models.Variable.objects.create(value=None, name="V_NONE1")
+        models.Variable.objects.create(name="V_NONE2")
+
+    @pytest.mark.it("Retrieve with all types")
+    def test_retrieve(self, variable_factory):
+        v_int = variable_factory(5, "V_INT")
+        v_float = variable_factory(5.5, "V_FLOAT")
+        v_bool = variable_factory(True, "V_BOOL")
+        v_str = variable_factory("bar", "V_STR")
+        v_none = variable_factory(None, "V_NONE1")
+
+        assert v_int.value == 5
+        assert v_float.value == 5.5
+        assert v_bool.value
+        assert v_str.value == "bar"
+        assert v_none.value is None
